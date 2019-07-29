de7c69f164db3ce892e1707a77995b6f8547e99e
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-07-29 18:41:09 -0400

    added movie-this functionality to CLI app

--- liri.js
+++ liri.js
@@ -1,19 +1,64 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-console.log(keys.spotify)
+var axios = require("axios");
+
 var spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+if (process.argv[2] === "movie-this"){
+
+    var userQuery = "Mr. Nobody";
+    var nodeArg = process.argv;
+    console.log("process argv at 3: " + process.argv[3]);
+    if (process.argv[3] !== "undefined") {
+        for ( i = 3 ; i < nodeArg.length ; i++ ) {
+            userQuery += (nodeArg[i] + " ");
+        };
     }
-//    for (i = 0; i < data.tracks.href.items.length ; i ++) {
-//        console.log(data.tracks.href.items[i]);
-//    }
-  console.log(data.tracks.items[0].name); 
-  });
+
+
+    axios.get("http://www.omdbapi.com/?t="+ userQuery +"&y=&plot=short&apikey=f8b737f3").then(
+    function(response) {
+        console.log("-----------------------");
+        console.log("Your Searched Movie \n");
+        console.log("The movie's title is: " + response.data.Title);
+        console.log("The year of release: " + response.data.Year);
+        console.log("The movie's Metascore: " + response.data.Metascore);
+        console.log("The movie's imbd rating is: " + response.data.imdbRating);
+        console.log("The movie was produced in: " + response.data.Country);
+        console.log("The movie's language is: " + response.data.Language);
+        console.log("The movie's actors: " + response.data.Actors);
+        console.log("The movie's plot: " + response.data.Plot);
+    })
+    .catch(function(error) {
+        console.log("There is an error in the axios function.")
+    });
+}
+
+if (process.argv[2] === "spotify-this-song") {
+    var userQuery = "";
+    var nodeArg = process.argv;
+    for ( i = 3 ; i < nodeArg.length ; i++ ) {
+        userQuery += (nodeArg[i] + " ");
+    };
+
+    spotify.search({ type: 'track', query: userQuery }, function(err, data) {
+        if (err) {
+        return console.log('Error occurred: ' + err);
+        }
+            for (i = 0; i < data.tracks.items.length ; i ++) {
+                console.log("------------------------------------------------------")
+                console.log("   ");
+                console.log("The name of the Track is: " + data.tracks.items[i].name);
+                console.log("The name of the Artist is: " + data.tracks.items[i].artists[0].name);
+                console.log("The preview link is: " + data.tracks.items[i].external_urls.spotify);
+                console.log("This song is from the album: " + data.tracks.items[i].album.name)
+                console.log("   ");
+            }
+    });
+}
+
+
 
 
 // concert-this
